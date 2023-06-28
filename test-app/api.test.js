2572205a55dd5876e78c6fc400b21caf16fa1825
Tobby Makinde <makinde.tobiloba@gmail.com>
2023-06-28 11:48:56 +0100

    Fix: Babel error fixed

--- test-app/api.test.js
+++ test-app/api.test.js
@@ -3,7 +3,11 @@ const { fetchTeacher, fetchStudents, createStaff, createStudent } = require('./a
 
 
 
-test('fetchStaff should return an array of teachers', async () => {
+test('fetchTeacher should return an array of teachers', async () => {
     const teachers = await fetchTeacher();
     expect(Array.isArray(teachers)).toBe(true);
+});
+test('fetchStudents should return an array of students', async () => {
+    const students = await fetchStudents();
+    expect(Array.isArray(students)).toBe(true);
 });
